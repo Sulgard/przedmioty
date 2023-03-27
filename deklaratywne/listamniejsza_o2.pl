e04ccba0f1e80a064cd7dd6b9e59d8d515effe1e
Artur Sutuła <73705012+Sulgard@users.noreply.github.com>
2023-03-27 13:55:42 +0200

    Create listamniejsza_o2.pl

---
+++ deklaratywne/listamniejsza_o2.pl
@@ -0,0 +1,15 @@
+% listamniejsza_o2(L1,L2).
+% spełniony, gdy elementy listy L2 są odpowiednimi
+% elementami listy L1 pomniejszonymi o 2, np. L1=[3,4], L2=[1,2] wynik [1,2]
+% -----------------------------------------------
+
+% warunek kończący rekurencję: podwojenie listy pustej
+% jest lista pustą.
+
+listamniejsza_o2([],[]).
+
+% rekurencja:
+
+listamniejsza_o2([H1|T1],[H2,T2]):-
+		H2 is H1-2,
+		listamniejsza_o2(T1,T2).
