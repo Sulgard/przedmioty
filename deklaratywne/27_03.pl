b9f473406dd56c59c73c88db62fbf998f5488fcc
Artur Sutuła <73705012+Sulgard@users.noreply.github.com>
2023-03-27 13:29:22 +0200

    Update 27_03.pl

--- deklaratywne/27_03.pl
+++ deklaratywne/27_03.pl
@@ -1,1 +1,32 @@
 
+// zadanie na co jest czym - stała, zmienna itd.
+// drzewo
+
+/*
+
+listy [1,2,3]
+
+H = 1 
+T = [2, 3]
+
+[X, Y | T]
+[a, b, c, d]
+X = a, Y = b, T = [c, d]
+[[a,b], [c], d] [X, Y | T]
+X = [a,b]
+Y = [c]
+T = [d]
+[X | T]   [[1,2]]
+
+X = [1,2]
+T = []
+
+[X,f(c),b] [g(f(Y)), Z, Y]
+X = g(f(b)) bo za Y będziemy podstawiać b później
+Z = f(c)
+Y = b
+
+
+
+
+*/
