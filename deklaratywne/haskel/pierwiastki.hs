210b2f6ece65adbb4dd1c6aa5f98d4f63b6879d5
Artur Sutuła <73705012+Sulgard@users.noreply.github.com>
2023-05-15 13:43:06 +0200

    Update pierwiastki.hs

--- deklaratywne/haskel/pierwiastki.hs
+++ deklaratywne/haskel/pierwiastki.hs
@@ -1,4 +1,4 @@
-pierwiastki::Int->Int->Int->String
+pierwiastki::float->float->float->String
 pierwiastki a b c
  |((b^2)+((-4)*a*c))>0 ="Dwa pierwiastki"
  |((b^2)+((-4)*a*c))<0 ="Zero pierwiastkow"
